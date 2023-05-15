2b186135fc27eb9b7944dc489e4489570ea412f5
Daniel Boye <83395536+DanielBoye@users.noreply.github.com>
2023-05-15 08:24:03 +0200

    Update indent

--- Arduino/Daniel/TrafikklysKnapp.ino
+++ Arduino/Daniel/TrafikklysKnapp.ino
@@ -6,7 +6,7 @@ int red2 = 13;
 int yellow2 = 12; 
 int green2 = 11; 
 void setup() { 
-	// light one 
+// light one 
 	pinMode(red1, OUTPUT); 
 	pinMode(yellow1, OUTPUT); 
 	pinMode(green1, OUTPUT); 
@@ -22,28 +22,28 @@ void loop() {
 }
 
 void changeLights() { 
-  // turn both yellows on 
-  digitalWrite(green1, LOW); 
-  digitalWrite(yellow1, HIGH); 
-  digitalWrite(yellow2, HIGH); 
-  delay(5000); 
-  // turn both yellows off, and opposite green and red 
-  digitalWrite(yellow1, LOW); 
-  digitalWrite(red1, HIGH); 
-  digitalWrite(yellow2, LOW); 
-  digitalWrite(red2, LOW); 
-  digitalWrite(green2, HIGH); 
-  delay(5000); 
-  // both yellows on again 
-  digitalWrite(yellow1, HIGH); 
-  digitalWrite(yellow2, HIGH); 
-  digitalWrite(green2, LOW); 
-  delay(3000); 
-  // turn both yellows off, and opposite green and red 
-  digitalWrite(green1, HIGH); 
-  digitalWrite(yellow1, LOW); 
-  digitalWrite(red1, LOW); 
-  digitalWrite(yellow2, LOW); 
-  digitalWrite(red2, HIGH); 
-  delay(5000); 
+	// turn both yellows on 
+	digitalWrite(green1, LOW); 
+	digitalWrite(yellow1, HIGH); 
+	digitalWrite(yellow2, HIGH); 
+	delay(5000); 
+	// turn both yellows off, and opposite green and red 
+	digitalWrite(yellow1, LOW); 
+	digitalWrite(red1, HIGH); 
+	digitalWrite(yellow2, LOW); 
+	digitalWrite(red2, LOW); 
+	digitalWrite(green2, HIGH); 
+	delay(5000); 
+	// both yellows on again 
+	digitalWrite(yellow1, HIGH); 
+	digitalWrite(yellow2, HIGH); 
+	digitalWrite(green2, LOW); 
+	delay(3000); 
+	// turn both yellows off, and opposite green and red 
+	digitalWrite(green1, HIGH); 
+	digitalWrite(yellow1, LOW); 
+	digitalWrite(red1, LOW); 
+	digitalWrite(yellow2, LOW); 
+	digitalWrite(red2, HIGH); 
+	delay(5000); 
 }
