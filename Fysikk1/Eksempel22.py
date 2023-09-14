6d7047771a6351b0552344b05287bac93ccdfe41
RohaanMooken <rohaanmooken@gmail.com>
2023-09-14 14:29:47 +0200

    Create Eksempel22.py

---
+++ Fysikk1/Eksempel22.py
@@ -0,0 +1,24 @@
+#Side 51
+
+import matplotlib.pyplot as plt
+
+s = 0
+v = 0
+t = 0
+
+s_slutt = 100
+dt = 1E-3
+s_verdier = []
+v_verdier = []
+
+def a(v):
+    return -0.5*v + 6
+
+while s < s_slutt:
+    v += a(v)*dt
+    s += v*dt
+    s_verdier.append(s)
+    v_verdier.append(v)
+
+plt.plot(s_verdier, v_verdier)
+plt.show()
