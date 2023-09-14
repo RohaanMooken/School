0a2e264d5bb10f0e13f8addb50c7ff520540298f
DanielBoye <danielboye888@gmail.com>
2023-09-14 11:03:57 +0200

    Fiks lol

--- Kjemi/1.4.29/main.py
+++ Kjemi/1.4.29/main.py
@@ -3,8 +3,8 @@ print("Tast 1 for ikke-metall og 2 for metall")
 stoff1 = int(input("Stoff 1: "))
 stoff2 = int(input("Stoff 2: "))
 if stoff1 == 1 and stoff2 == 1:
-    print("Dette blir en ionebinding.")
-elif stoff1 == 1 and stoff2 == 2:
-    print("Dette blir en kovalent binding.")
+    print("Dette blir en kovalent.")
+elif stoff1 == 1 and stoff2 == 2 or stoff1 == 2 and stoff2 == 1:
+    print("Dette blir en inonebinding.")
 else:
     print("Dette blir en metallbinding.")
