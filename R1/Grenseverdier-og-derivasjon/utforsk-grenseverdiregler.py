fa712b122cce5fccbfaddc22f8c074fa645e4b33
DanielBoye <danielboye888@gmail.com>
2023-09-29 08:36:57 +0200

    Create utforsk-grenseverdiregler.py

---
+++ R1/Grenseverdier-og-derivasjon/utforsk-grenseverdiregler.py
@@ -0,0 +1,15 @@
+def f(x): 
+    return 3*x - 6 
+
+svar = input("Velg verdi for a: ") 
+a = float(svar) 
+
+t = 0.1 
+while t > 0.0000001: 
+    print("f(" + str(round(a - t, 7)) + ") =", round(f(a - t), 7)) 
+    t = t/10 
+
+t = 0.1 
+while t > 0.0000001: 
+    print("f(" + str(round(a + t, 7)) + ") =", round(f(a + t), 7)) 
+    t = t/10 
