c03320e561b0deb2c65053af0d6274209bdb0ee6
DanielBoye <danielboye888@gmail.com>
2023-10-19 14:44:11 +0200

    Create 2.73.py

---
+++ Fysikk1/1E-Simulering-Av-Bevegelse/2.73.py
@@ -0,0 +1,39 @@
+import matplotlib.pyplot as plt
+
+def a(v): 
+    sumkrefter=m*g-k*v**2 
+    akselerasjon=sumkrefter/m 
+    return akselerasjon 
+
+m = 46 * 10
+g = 9.81
+dt = 0.01
+
+s = 0
+v = 32
+t = 0
+
+k = 0.3
+
+t_verdier = []
+s_verdier = []
+v_verdier = []
+
+t_verdier.append(t)
+s_verdier.append(s)
+v_verdier.append(v)
+
+while t < 100:
+    t = t + dt
+    v = v + a(v) * dt
+    s = s + v * dt
+    t_verdier.append(t)
+    s_verdier.append(s)
+    v_verdier.append(v)
+
+
+print(round(s, 2), round(t, 2))  
+print("terminal", v)  
+plt.plot(t_verdier, v_verdier)
+plt.show()
+plt.grid()    
