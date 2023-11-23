90850672526de0b59184d2859a0cd5068ad45cbd
DanielBoye <danielboye888@gmail.com>
2023-11-23 10:59:01 +0100

    Create main.py

---
+++ Kjemi/4.2.8/main.py
@@ -0,0 +1,25 @@
+atom1 = []
+atom2 = []
+bindingsorden = []
+reaksjonsentalpi = []
+
+with open('bindingsentalpi.txt', 'r') as file:
+    next(file)
+    for line in file:
+        parts = line.split()
+        atom1.append(parts[0].strip())
+        atom2.append(parts[1].strip())
+        bindingsorden.append(parts[2].strip())
+        reaksjonsentalpi.append(parts[3].strip())
+
+a1 = input("Første atom: ").strip()
+a2 = input("Andre atom: ").strip()
+orden = input("Bindingsorden: ").strip()
+
+for i in range(len(atom1)):
+    current_atom1 = atom1[i]
+    current_atom2 = atom2[i]
+    current_bindingsorden = bindingsorden[i]
+
+    if current_atom1 == a1 and current_atom2 == a2 and current_bindingsorden == orden:
+        print("Reaksjonsentalpien er:", reaksjonsentalpi[i], "kJ.")
