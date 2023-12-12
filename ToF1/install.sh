8533da2991785e77e0b5e87d1a5be2973da1124e
DanielBoye <danielboye888@gmail.com>
2023-12-12 12:12:57 +0100

    Create install.sh

---
+++ ToF1/install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+sudo apt install libjpeg-dev libtiff-dev libjasper-dev libpng-dev libwebp-dev libopenexr-dev -y
+
+sudo apt install libavcodec-dev libavformat-dev libswscale-dev libv4l-dev libxvidcore-dev libx264-dev libdc1394-22-dev libgstreamer-plugins-base1.0-dev libgstreamer1.0-dev -y
+
+sudo apt install libgtk-3-dev libqtgui4 libqtwebkit4 libqt4-test python3-pyqt5 -y
+
+sudo apt install libatlas-base-dev liblapacke-dev gfortran-y
+
+sudo apt install libhdf5-dev libhdf5-103 -y
+
+sudo apt install python3-dev python3-pip python3-numpy -y
+
